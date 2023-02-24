ffcb2f1eec56941c9f3ec0144c2c968c8f92c6eb
helbing <helbingxxx@gmail.com>
2023-02-24 15:43:33 +0800

    feat: add monorepo

--- apps/web/lib/cdk-app.ts
+++ apps/web/lib/cdk-app.ts
@@ -1,28 +1,14 @@
 import { Duration, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib"
 import * as s3 from "aws-cdk-lib/aws-s3"
 import { Construct } from "constructs"
+import { ImageHandler } from "@las/cdk-image-handler"
 
 export default class CDKApp extends Stack {
-  private readonly resizeImageFunctionName: string =
-    process.env.LAMBDA_RESIZE_IMAGE ||
-    "helbing-learn-aws-serverless-resize-image"
-
-  private readonly uploadBucketName: string =
-    process.env.BUCKET_UPLOAD_IMAGE ||
-    "helbing-learn-aws-serverless-upload-image"
-
-  private readonly resizedBucketName: string =
-    this.uploadBucketName + "-resized"
-
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
-    this.resizeImage()
-  }
-  private resizeImage() {
-    const uploadBucket = new s3.Bucket(this, this.uploadBucketName, {
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
+    const imageHandler = new ImageHandler(this, "fasd", {
+      bucketName: "exam",
     })
   }
 }
--- packages/cdk-image-handler/image-handler.ts
+++ packages/cdk-image-handler/image-handler.ts
@@ -2,7 +2,7 @@ import { Duration, RemovalPolicy } from "aws-cdk-lib"
 import * as s3 from "aws-cdk-lib/aws-s3"
 import { Construct } from "constructs"
 
-export interface S3ToLambdaStackProps {
+export interface ImageHandlerStackProps {
   /**
    * The bucket name which will trigger event to lambada
    *
@@ -11,11 +11,11 @@ export interface S3ToLambdaStackProps {
   readonly bucketName: string
 }
 
-export class S3ToLambda extends Construct {
-  constructor(scope: Construct, id: string, props?: S3ToLambdaStackProps) {
+export class ImageHandler extends Construct {
+  constructor(scope: Construct, id: string, props?: ImageHandlerStackProps) {
     super(scope, id)
 
-    const bucket = new s3.Bucket(this, props?.bucketName || "example", {
+    const bucket = new s3.Bucket(this, props?.bucketName || "demo", {
       removalPolicy: RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
     })
