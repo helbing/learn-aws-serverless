a6090eff70af0912c9a51a0adf01a8cac29ae1e8
helbing <helbingxxx@gmail.com>
2023-03-04 15:56:22 +0800

    test: update jest snapshot

--- snapshotResolver.ts
+++ snapshotResolver.ts
@@ -1,9 +1,11 @@
-import { SnapshotResolver, isSnapshotPath } from "jest-snapshot"
+import { SnapshotResolver } from "jest-snapshot"
 
 const resolver: SnapshotResolver = {
   testPathForConsistencyCheck: "some/example.test.ts",
-  resolveSnapshotPath: (testPath, snapshotExtension) => testPath,
-  resolveTestPath: (snapshotFilePath) => snapshotFilePath,
+  resolveSnapshotPath: (testPath, snapshotExtension) =>
+    testPath + snapshotExtension,
+  resolveTestPath: (snapshotFilePath, snapshotExtension) =>
+    snapshotFilePath.slice(0, -(snapshotExtension as string).length),
 }
 
 export default resolver
